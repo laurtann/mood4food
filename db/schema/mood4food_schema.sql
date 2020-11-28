fd61232059edde880050132eeb858312cdb0996b
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-28 16:40:44 +0000

    adding schema and erd food url name update

--- db/schema/mood4food_schema.sql
+++ db/schema/mood4food_schema.sql
@@ -1,23 +1,27 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS food_items CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   phone_num VARCHAR(32) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password varchar(255) not null
+  password varchar(255) NOT NULL
 )
 
 CREATE TABLE food_items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  cost integer not null,,
-  description text not null,
-  in_stock bolean not null
-  food_photo_url varchar(255) not null
+  cost integer NOT NULL,
+  description text NOT NULL,
+  in_stock bolean NOT NULL,
+  food_photo_url varchar(255) NOT NULL
 )
 
 CREATE TABLE orders(
-	id serial primary key,
-	user_id integer not null references users(id) on delete cascade,
-	food_id integer not null references food_items(id) on delete cascade
+	id SERIAL PRIMARY KEY NOT NULL,
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	food_id integer NOT NULL REFERENCES food_items(id) ON DELETE CASCADE
 )
