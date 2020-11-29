86ce98f37337065aa5fa141206b25fe157f0e45d
Laura Tannahill <lntannahill@gmail.com>
2020-11-29 19:38:56 +0000

    Create basic functions for dynamic qty handling

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,1 +1,22 @@
-document.getElementById("plus").addEventListener("click", console.log("clicked"));
+
+
+$(document).ready(function() {
+  let quantityValue = parseInt($("#qty").text());
+
+  $("#plus").click(function() {
+    quantityValue += 1;
+    $("#qty").text(String(quantityValue));
+  });
+
+  $("#minus").click(function() {
+    if (quantityValue > 0) {
+      quantityValue -= 1;
+    }
+    $("#qty").text(String(quantityValue));
+  });
+
+  $("#remove").click(function() {
+    quantityValue = 0;
+    $("#qty").text(String(quantityValue));
+  });
+});
