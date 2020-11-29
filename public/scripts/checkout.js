f31a9869c56b55ad70cfbff066e24d4559772804
Laura Tannahill <lntannahill@gmail.com>
2020-11-29 21:58:29 +0000

    Simplified checkout page and added logic to adjust price for quantity of items

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -3,23 +3,41 @@ $(document).ready(function() {
   $(".plus").click(function() {
     let quantity = $(this).parent().siblings(".qty");
     let quantityValue = parseInt($(quantity).text());
+
+    let costPerItem = $(this).parent().siblings(".item-cost").text();
+    let totalItemCost = $(this).parent().siblings(".item-total");
+
+    let itemCost = Number(costPerItem.replace(/[\.$]+/g, ""));
+
     quantityValue += 1;
+    let totalCost = itemCost * quantityValue;
+
     $(quantity).text(String(quantityValue));
+    $(totalItemCost).text("$" + String(totalCost / 100));
+
   });
 
   $(".minus").click(function() {
     let quantity = $(this).parent().siblings(".qty");
     let quantityValue = parseInt($(quantity).text());
+
+    let costPerItem = $(this).parent().siblings(".item-cost").text();
+    let totalItemCost = $(this).parent().siblings(".item-total");
+
+    let itemCost = Number(costPerItem.replace(/[\.$]+/g, ""));
     if (quantityValue > 0) {
       quantityValue -= 1;
     }
+    let totalCost = itemCost * quantityValue;
     $(quantity).text(String(quantityValue));
+    $(totalItemCost).text("$" + String(totalCost / 100));
   });
 
   $(".remove").click(function() {
     let quantity = $(this).parent().siblings(".qty");
-    let quantityValue = parseInt($(quantity).text());
-    quantityValue = 0;
-    $(quantity).text(String(quantityValue));
+    let totalItemCost = $(this).parent().siblings(".item-total");
+
+    $(quantity).text("0");
+    $(totalItemCost).text("$0");
   });
 });
