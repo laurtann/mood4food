4094cc9d490d7c30a4e32c8d032d627f44280945
Laura Tannahill <lntannahill@gmail.com>
2020-11-29 19:53:59 +0000

    Create helper function for code readability

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,22 +1,23 @@
 
+const valToString = (qty) => $("#qty").text(String(qty));
 
 $(document).ready(function() {
   let quantityValue = parseInt($("#qty").text());
 
   $("#plus").click(function() {
     quantityValue += 1;
-    $("#qty").text(String(quantityValue));
+    valToString(quantityValue);
   });
 
   $("#minus").click(function() {
     if (quantityValue > 0) {
       quantityValue -= 1;
     }
-    $("#qty").text(String(quantityValue));
+    valToString(quantityValue);
   });
 
   $("#remove").click(function() {
     quantityValue = 0;
-    $("#qty").text(String(quantityValue));
+    valToString(quantityValue);
   });
 });
