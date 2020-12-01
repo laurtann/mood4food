1eeda72b3d44cece154c21a570171ebe21ca3251
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-12-01 21:50:43 +0000

    adding seeds with real data and populating on the browser

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -1,12 +1,12 @@
 -- SEEDS FOR ORDERS TABLE
 
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (1, 26, 20, 1, 'ip');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (2, 5, 28, 2, 'accepted');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (3, 46, 25, 1, 'completed');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (4, 10, 45, 1, 'ip');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (5, 31, 40, 4, 'accepted');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (6, 40, 47, 1, 'completed');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (7, 24, 13, 1, 'completed');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (8, 29, 12, 3, 'accepted');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (9, 39, 27, 1, 'completed');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (10, 1, 33, 2, 'ip');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (1, 1, 8, 1, 'ip');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (2, 5, 7, 2, 'accepted');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (3, 1, 5, 1, 'completed');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (4, 3, 6, 1, 'ip');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (5, 4, 8, 4, 'accepted');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (6, 2, 3, 1, 'completed');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (7, 6, 4, 1, 'completed');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (8, 7, 1, 3, 'accepted');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (9, 1, 2, 1, 'completed');
+INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (10, 1, 2, 2, 'ip');
