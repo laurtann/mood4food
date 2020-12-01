5157f33f4657f99e5075254a8d8fefdf89918a33
Julia Di Monte <dimontejulia@gmail.com>
2020-12-01 19:40:52 +0000

    restaurant sms working

--- twilio-sms-api/index.js
+++ twilio-sms-api/index.js
@@ -3,14 +3,19 @@
 
 const express = require("express");
 const app = express();
+const http = require("http");
+const bodyParser = require("body-parser");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/sms", function (req, res) {
+app.get("/", function (req, res) {
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require("twilio")(accountSid, authToken);
   client.messages
     .create({
-      body: "This is the initial greeting message",
+      body:
+        "Order number 101 has been placed. Please update esimated pickup time.",
       from: "+12055966681",
       to: "+16473823731",
     })
@@ -21,4 +26,18 @@ app.get("/sms", function (req, res) {
     );
 });
 
-app.listen(3000);
+app.post("/sms", (req, res) => {
+  const twiml = new MessagingResponse();
+
+  //sms response stored in req.body.Body
+  twiml.message(
+    `Thank you for updating us. The customer has been notified that the estimated pickup time is in ${req.body.Body} minutes.`
+  );
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(3000, () => {
+  console.log("Express server listening on port 3000");
+});
