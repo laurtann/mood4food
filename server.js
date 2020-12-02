5e34da206ae88a8d80fce1f6beeb595ea18bedf7
Julia Di Monte <dimontejulia@gmail.com>
2020-12-02 02:27:13 +0000

    working messaging

--- server.js
+++ server.js
@@ -130,8 +130,6 @@ app.post("/sms", (req, res) => {
   );
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
-
-  res.render("/confirm", { orderTime: orderTime });
 });
 
 //jpiotrowski0@jigsy.com --> password
