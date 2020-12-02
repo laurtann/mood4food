11dae0211e2c006a3ef7ea5f673a7ace8d583f56
Julia Di Monte <dimontejulia@gmail.com>
2020-12-02 03:21:26 +0000

    send message to multiple numbers

--- server.js
+++ server.js
@@ -109,15 +109,21 @@ app.get("/confirm", function (req, res) {
   console.log("in orders");
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const numbers = ["+14169869248", "+16473823731"];
   const client = require("twilio")(accountSid, authToken);
-  client.messages
-    .create({
-      body:
-        "Order number 101 has been placed. Please update esimated pickup time.",
-      from: "+12055966681",
-      to: "+16473823731",
-    })
-    .then((message) => res.render("confirmation", { message: message }));
+
+  numbers.forEach(async (number) => {
+    const message = await client.messages
+      .create({
+        body:
+          "The order has been sucessfully placed. Delivery time will be updated shortly.",
+        from: "+12055966681",
+        to: number,
+      })
+      .then((message) => {
+        res.render("confirmation", { message: message });
+      });
+  });
 });
 
 app.post("/sms", (req, res) => {
