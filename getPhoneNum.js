91cde16ce66e8cc324ab119d9b0220af8005936f
Laura Tannahill <lntannahill@gmail.com>
2020-12-03 06:17:45 +0000

    Add template var

--- getPhoneNum.js
+++ getPhoneNum.js
@@ -7,13 +7,13 @@ const db = new Pool(dbParams);
 db.connect();
 
 module.exports = (db) => {
-  const getPhoneNumFromId = () => {
+  const getPhoneNumFromId = (phoneNumber) => {
     return db
       .query(
         `
       SELECT phone_num from users
-      WHERE users.id = 1;
-      ;`
+      WHERE users.id = $1;
+      ;`, [phoneNumber]
       )
       .then((response) => {
         return response.rows[0];
