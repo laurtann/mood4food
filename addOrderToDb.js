fa4336acc6d941a82d33e2d299795833cc5f88ab
Laura Tannahill <lntannahill@gmail.com>
2020-12-03 21:09:07 +0000

    remove console log

--- addOrderToDb.js
+++ addOrderToDb.js
@@ -6,24 +6,6 @@ const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
-// const generateRandomNumber = () => {
-//   const random = Math.floor(Math.random() * 1000) + 61;
-//   return random;
-// };
-
-// module.exports.addToOrderDb = (nameAndQty, orderNote, total, userId, orderStatus) => {
-//   return db
-//     .query(
-//     `
-//     INSERT INTO orders
-//     VALUES ($1, $2, $3, $4, $5, $6)
-//     RETURNING *;
-//     `, [generateRandomNumber(), nameAndQty, orderNote, total, userId, orderStatus]
-//     )
-//     .then(res => res.rows[0])
-//     .catch(err => null);
-//   };
-
   module.exports.addToOrderDb = (orderNumber, nameAndQty, total, orderNote, orderStatus, userId) => {
     return db
     .query(
