d2ed7f803a7d638366e0f2d1a53804ce5e262704
Laura Tannahill <lntannahill@gmail.com>
2020-12-03 06:06:54 +0000

    Removed prototype seeds from orders, added one with current params for testing

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -1,12 +1,4 @@
 -- SEEDS FOR ORDERS TABLE
 
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (1, 1, 8, 1, 'ip');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (2, 5, 7, 2, 'accepted');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (3, 1, 5, 1, 'completed');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (4, 3, 6, 1, 'ip');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (5, 4, 8, 4, 'accepted');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (6, 2, 3, 1, 'completed');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (7, 6, 4, 1, 'completed');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (8, 7, 1, 3, 'accepted');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (9, 1, 2, 1, 'completed');
-INSERT INTO orders (id, user_id, food_id, food_qty, order_status) VALUES (10, 1, 2, 2, 'ip');
+INSERT INTO orders (id, food_name_qty, order_total, order_notes, order_status, user_id) VALUES (1, 'food: 1', '$8', 'extra pickle', 'ip', 1);
+
