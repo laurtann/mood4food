d9489c7080e384fc0af430a6650756e88aff0c50
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-12-03 20:55:38 +0000

    working code for registeration page

---
+++ 5_check_existing_email_reg.js
@@ -0,0 +1,18 @@
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+module.exports.fetchEmailId = (email) => {
+  const sql =
+  `
+  SELECT *
+  FROM users where email = $1;
+  `;
+  const params = [email];
+  return db.query(sql,params)
+      .then((response) => {
+        console.log('response.rows[0] : ',response.rows[0]);
+        return response.rows[0].email;
+      })
+      .catch((err)=>{console.log('query error', err);});
+  };
